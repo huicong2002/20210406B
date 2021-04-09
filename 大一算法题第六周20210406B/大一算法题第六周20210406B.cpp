b387c7c912b1d19f9d16c10cc1b480ee457a7d60
huicong2002 <1532751400@qq.com>
2021-04-09 15:29:11 +0800

    logs

---
+++ 大一算法题第六周20210406B/大一算法题第六周20210406B.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <stdio.h>
+
+int n;
+int arr[100005], sum[100005];
+
+int main() {
+    int max = 0;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &arr[i]);
+        if (arr[i] == 0)
+        {
+            arr[i] = -1;
+        }
+        sum[i] = sum[i - 1] + arr[i];
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (sum[i] == sum[j]) {
+                if (i - j > max) {
+					max = i - j;
+				}
+                break;
+            }
+        }
+    }
+
+    printf("%d", max);
+
+    return 0;
+}
